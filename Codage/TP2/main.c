cd79075444d9df2ca866bd2000c4c84bf29e8e24
Percedal <moi.durand.72@gmail.com>
2019-01-07 15:17:23 +0100

    l'utilisateur choisi le nombre de nombre pseu-aléa qu'il veut generer

--- Codage/TP2/main.c
+++ Codage/TP2/main.c
@@ -9,15 +9,24 @@
 
 
 int main(/*int argc, char const *argv[]*/) {
+	int nbNombre = 0;
 	ptrVecteur_t vec;
 
 
+	do {
+		printf("Combien de nombres souhaitez-vous générer ? ");
+		scanf("%d", &nbNombre);
+		if(nbNombre <= 0)
+			printf("Oups, la valeur est <= à 0 !\n");
+	} while(nbNombre <= 0);
 
 
-	printf("\n---- Codeur à longeur maximale ----\n\nGénération de 10 nombres pseudo-aléatoires :\n");
+
+
+	printf("\n---- Codeur à longeur maximale ----\n\nGénération de %d nombres pseudo-aléatoires :\n", nbNombre);
 
 	ptrCodeLongMax_t lm = creerCodeLongMax("[16, 14, 13, 11]", "1");
-	for(int i = 0; i < 10; i++) {
+	for(int i = 0; i < nbNombre; i++) {
 		vec = genererSequence(lm, 8*sizeof(int));
 		printf("\n\t%d\n", vecToInt(vec));
 		detruireVecteur(&vec);
@@ -27,11 +36,11 @@ int main(/*int argc, char const *argv[]*/) {
 
 
 
-	printf("\n\n\n---- Codeur de Gold ----\n\nGénération de 10 nombres pseudo-aléatoires :\n");
+	printf("\n\n\n---- Codeur de Gold ----\n\nGénération de %d nombres pseudo-aléatoires :\n", nbNombre);
 
 	struct gold gold;
 	initialiserGold(&gold, "[16, 14, 13, 11]");
-	for(int i = 0; i < 10; i++) {
+	for(int i = 0; i < nbNombre; i++) {
 		vec = genererGold(gold, 8*sizeof(int));
 		printf("\n\t%d\n", vecToInt(vec));
 		detruireVecteur(&vec);
@@ -40,13 +49,13 @@ int main(/*int argc, char const *argv[]*/) {
 
 
 
-	printf("\n\n\n---- Codeur JPL ----\n\nGénération de 10 nombres pseudo-aléatoires :\n");
+	printf("\n\n\n---- Codeur JPL ----\n\nGénération de %d nombres pseudo-aléatoires :\n", nbNombre);
 
 	struct jpl jpl;
 
 	initialiserJpl(&jpl, 3, "[2, 1]", "[3, 1]", "[5, 1]");
 
-	for(int i = 0; i < 10; i++) {
+	for(int i = 0; i < nbNombre; i++) {
 		vec = genererJpl(jpl, 8*sizeof(int));
 		printf("\n\t%d\n", vecToInt(vec));
 		detruireVecteur(&vec);
